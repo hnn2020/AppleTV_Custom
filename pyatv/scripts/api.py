87e7003510f2e9c7553772bf50a4960ca177d3b2
hnn2020 <hananel301294@gmail.com>
2025-04-30 11:02:55 +0300

    Fix pyatv directory to include actual files instead of submodule reference

---
+++ pyatv/scripts/api.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+"""Simple tool to generate API reference documentation."""
+import argparse
+from difflib import unified_diff
+import os
+from pathlib import Path
+import sys
+import warnings
+
+import pdoc
+
+warnings.filterwarnings("ignore")
+pdoc.tpl_lookup.directories.insert(0, os.path.join("docs", "pdoc_templates"))
+
+
+def _filter_func(doc):
+    for exclude in [
+        "auth",
+        "core",
+        "protocols",
+        "raop",
+        "scripts",
+        "support",
+    ]:
+        if doc.refname.startswith("pyatv." + exclude):
+            return False
+    return True
+
+
+def _api_modules():
+    def _recursive_htmls(mod):
+        yield mod.name, mod.html()
+        for submod in mod.submodules():
+            yield from _recursive_htmls(submod)
+
+    modules = ["pyatv"]
+    context = pdoc.Context()
+
+    modules = [
+        pdoc.Module(mod, context=context, docfilter=_filter_func) for mod in modules
+    ]
+    pdoc.link_inheritance(context)
+
+    for mod in modules:
+        for module_name, html in _recursive_htmls(mod):
+            output_file = os.path.join("docs", "api", module_name + ".html")
+            yield output_file, html
+
+
+def generate():
+    """Generate API reference."""
+    for output_file, html in _api_modules():
+        if not os.path.exists(os.path.dirname(output_file)):
+            os.mkdir(os.path.dirname(output_file))
+        print("Generating", output_file)
+        with open(output_file, "wb") as f:
+            f.write(html.encode("utf-8"))
+
+    return 0
+
+
+def verify():
+    """Verify that API reference is up-to-date."""
+    return_value = 0
+    for output_file, html in _api_modules():
+        print("Verifying", output_file)
+        expected_output = Path(output_file).read_text(encoding="utf-8")
+        if html != expected_output:
+            print(
+                "File content mismatch - run: api.py generate. Diff is below:",
+                file=sys.stderr,
+            )
+            print(
+                "".join(
+                    unified_diff(expected_output.splitlines(1), html.splitlines(1))
+                ),
+                file=sys.stderr,
+            )
+            return_value = 1
+    return return_value
+
+
+def main():
+    """Script starts here."""
+    if not os.path.exists(".git"):
+        print("Run this script from repo root", file=sys.stderr)
+        return 1
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "command", choices=["generate", "verify"], help="command to run"
+    )
+
+    args = parser.parse_args()
+
+    if args.command == "generate":
+        return generate()
+    if args.command == "verify":
+        return verify()
+
+    return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ pyatv/scripts/setup_dev_env.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Set in GitPod which breaks installation in venv
+unset PIP_USER
+
+VERSIONS="3.10 3.9 3.8 3.7 3.6"
+
+found_version=
+for p in $VERSIONS
+do
+    which python${p} 2>&1 > /dev/null
+    if [ $? -eq 0 ]; then
+        found_version=python${p}
+        break
+    fi
+done
+
+set -e
+
+if [ -z $found_version ]; then
+    >&2 echo "no python installation found"
+    exit 1
+fi
+
+echo "-> Using python: $found_version"
+
+echo "-> Creating python virtual environment..."
+$found_version -m venv .
+
+echo "-> Activating virtual environment..."
+source bin/activate
+sed -i '' 's/false/true/' pyvenv.cfg
+
+echo "-> Upgrading pip..."
+pip install --upgrade pip
+
+echo "-> Installing library as develop..."
+python setup.py develop
+
+echo "-> Installing protobuf-setuptools..."
+pip install protobuf-setuptools
+
+echo "-> Installing dependencies..."
+pip install --upgrade pyyaml -r requirements/requirements_test.txt -r requirements/requirements_docs.txt
+
+echo "-> Running tests as verification..."
+python setup.py test
+
+cat <<EOF
+==================================================
+
+When starting a new shell, run:
+
+  source bin/activate
+
+To run tests, run any of:
+
+  python setup.py test
+  pytest tests/test_conf.py  # Single test
+
+Test everything with chickn:
+
+  ./scripts/chickn.py
+
+To re-generate protobuf messages:
+
+  ./scripts/protobuf.py --download generate
+
+The CLI application can be used, e.g. run:
+
+  atvremote --debug commands
+  atvremote --debug playing
+
+To preview documentation in docs, run:
+
+  ./scripts/build_docs.sh
+
+and navigate to http://127.0.0.1:4000
+
+==================================================
+
+Environment is configured and ready to use!
+EOF
